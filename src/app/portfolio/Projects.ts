22b698bfd18296698749e653f6a48381abd5791f
Alvaro Torres <alvaro@alvarotorres.dev>
2025-11-02 23:25:00 -0600

    Added demo video to PulseDesk

--- src/app/portfolio/Projects.ts
+++ src/app/portfolio/Projects.ts
@@ -269,7 +269,7 @@ const projects = [
     summary: "AI-powered data analytics platform that detects anomalies in call center performance datasets.",
     description: "PulseDesk is an AI-powered analytics platform that automates anomaly detection and performance analysis for contact centers. The app was built with Python, FastAPI, and scikit-learn's Isolation Forest algorithm. Streamlining data processing and surfaces actionable insights through an interactive React dashboard powered by Plotly.js. PulseDesk was Designed to enhance operational visibility and accuracy with the success goal of improving reporting efficiency by over 45%.",
     image: "/images/projects/project_logos/pulsedesk.jpg",
-    page_images: ["/images/projects/project_pages/skipapi_landing.png", "/images/projects/project_pages/skipapi_app.png"],
+    page_images: [],
     techStack: [
       "Python",
       "FastAPI",
@@ -285,7 +285,7 @@ const projects = [
     github: "",
     figma: [],
     live: "",
-    demo: "",
+    demo: "https://www.youtube.com/embed/C9e6pvu6YVs?si=ppm1V4Vb-eR8R7bX",
     type: "Professional Work",
   },
 ];
